ed77e2a4b302eb23e031a72077235d0abd7da3a6
Vince Andrew Pimentel <contact@vincep.me>
2018-10-22 21:51:50 +0000

    automatically backed up by learn

---
+++ lib/dog.rb
@@ -0,0 +1,14 @@
+class Dog
+  attr_accessor :name
+
+  @@all = Array.new
+
+  def initialize(name)
+    @name = name
+    @all << name
+  end
+
+  def self.all
+    @@all.each { |name| puts "#{name}" }
+  end
+end
